f76e146744a4531c37d9ec361c2a9848472a06e3
dsabadis <dsabadis@CLJSDEDSABADI01>
2018-07-13 13:50:15 +0300

    chapter 5 setting up EF

---
+++ FriendOrganizer/FriendOrganizer.DataAccess/FriendOrganizerDbContext.cs
@@ -0,0 +1,39 @@
+﻿using FriendOrganizer.Model;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace FriendOrganizer.DataAccess
+{
+    public class FriendOrganizerDbContext : DbContext
+    {
+        public FriendOrganizerDbContext() : base("FriendOrganizerDb")
+        {
+
+        }
+
+        public DbSet<Friend> Friends { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            modelBuilder.Configurations.Add(new FriendConfiguration()); // if using data annotations (used for validation in UI) => no need for fluent config API
+            //modelBuilder.Entity<Friend>()
+            //    .Property(f => f.FirstName)
+            //    .IsRequired()
+            //    .HasMaxLength(50);
+        }
+    }
+
+    public class FriendConfiguration : EntityTypeConfiguration<Friend>
+    {
+        public FriendConfiguration()
+        {
+            Property(f => f.FirstName)
+                .IsRequired()
+                .HasMaxLength(50);
+        }
+    }
+}
