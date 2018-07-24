5e432c7d793d07f540098926fab0c65f4befdee3
DESKTOP-8OGCHC2\dan_s <dan.sabadas@gmail.com>
2018-07-24 22:04:06 +0300

    done chapter 10!

--- FriendOrganizer/FriendOrganizer.UI/ViewModel/NavigationItemViewModel.cs
+++ FriendOrganizer/FriendOrganizer.UI/ViewModel/NavigationItemViewModel.cs
@@ -1,25 +1,43 @@
-﻿namespace FriendOrganizer.UI.ViewModel
+﻿using FriendOrganizer.UI.Event;
+using Prism.Commands;
+using Prism.Events;
+using System.Windows.Input;
+
+namespace FriendOrganizer.UI.ViewModel
 {
-    public class NavigationItemViewModel : ViewModelBase
+  public class NavigationItemViewModel : ViewModelBase
+  {
+    private string _displayMember;
+    private IEventAggregator _eventAggregator;
+
+    public NavigationItemViewModel(int id, string displayMember,
+      IEventAggregator eventAggregator)
     {
-        private string _displayMember;
-
-        public NavigationItemViewModel(int id, string displayMember)
-        {
-            Id = id;
-            DisplayMember = displayMember;
-        }
-
-        public int Id { get; }
-
-        public string DisplayMember
-        {
-            get => _displayMember;
-            set
-            {
-                _displayMember = value;
-                OnPropertyChanged();
-            }
-        }
+      _eventAggregator = eventAggregator;
+      Id = id;
+      DisplayMember = displayMember;
+      OpenFriendDetailViewCommand = new DelegateCommand(OnOpenFriendDetailView);
     }
+
+    public int Id { get; }
+
+    public string DisplayMember
+    {
+      get => _displayMember;
+      set
+      {
+        _displayMember = value;
+        OnPropertyChanged();
+      }
+    }
+
+    public ICommand OpenFriendDetailViewCommand { get; }
+
+    private void OnOpenFriendDetailView()
+    {
+      _eventAggregator
+        .GetEvent<OpenFriendDetailViewEvent>()
+        .Publish(Id);
+    }
+  }
 }
