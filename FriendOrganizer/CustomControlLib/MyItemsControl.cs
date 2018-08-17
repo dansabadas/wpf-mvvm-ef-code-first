95750fd6d743ec84b435ce9c2a36455bfff3714a
dansabadas <dsabadis@sdl.com>
2018-08-17 17:36:50 +0300

    arrived at Demo-TemplatedBinding vs TemplatedParent

---
+++ FriendOrganizer/CustomControlLib/MyItemsControl.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace CustomControlLib
+{
+    public class MyItemsControl : ItemsControl
+    {
+        static MyItemsControl()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(MyItemsControl), new FrameworkPropertyMetadata(typeof(MyItemsControl)));
+        }
+    }
+}
