a04e8d6a7ad3395e9a4a266c90bfb0fc1f14a4d4
dansabadas <dsabadis@sdl.com>
2018-08-17 17:05:18 +0300

    arrived at ContentPresenter in chapter 2

---
+++ FriendOrganizer/CustomControlLib/MyContentControl.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace CustomControlLib
+{
+    public class MyContentControl : ContentControl
+    {
+        static MyContentControl()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(MyContentControl), new FrameworkPropertyMetadata(typeof(MyContentControl)));
+        }
+    }
+}
